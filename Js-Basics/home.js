1110ea260eac7e30c43daaf9fd2f7625a76b212b
IKECHI GEORGE ONUOBIA <ikechionuobia@gmail.com>
2022-06-25 13:20:49 +0100

    My Javascript Assignment
    It displays my name , height and my country of origin

---
+++ Js-Basics/home.js
@@ -0,0 +1,5 @@
+//This is my first javascript code!
+//This is my javascript assignment that displays My details!
+console.log('My name is = Ikechi George Onuobia');
+console.log('My height is = 1.78m');
+console.log('My Country of origin is = NIGERIA');
