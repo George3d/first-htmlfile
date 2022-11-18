288f9afc899ce3ba4023150a76f2751239228593
IKECHI GEORGE ONUOBIA <ikechionuobia@gmail.com>
2022-11-18 01:14:31 +0100

    page correction
    Slight changes

--- Js-Basics/home.js
+++ Js-Basics/home.js
@@ -3,3 +3,10 @@
 console.log('My name is = Ikechi George Onuobia');
 console.log('My height is = 1.78m');
 console.log('My Country of origin is = NIGERIA');
+
+//Variable name!
+let name = 'George';
+console.log(name);
+
+let house = 'Duplex';
+console.log(house);
